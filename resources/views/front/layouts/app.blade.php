fdf81ba501583d0bac39f4110b1ad37d859ae537
Meet Kapadiya <meetpatel5393@gmail.com>
2025-06-28 18:39:02 +0800

    Fixed jquery dependency issue

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
     <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">-->
 
-    <!--<link rel="stylesheet" href="{!! frontAssets('css/tiny-slider.css') !!}">-->
+    <link rel="stylesheet" href="{!! frontAssets('css/tiny-slider.css') !!}">
     <!--<link rel="stylesheet" href="{!! frontAssets('css/aos.css') !!}">-->
     <!--<link rel="stylesheet" href="{!! frontAssets('css/flatpickr.min.css') !!}">-->
     <!--<link rel="stylesheet" href="{!! frontAssets('css/glightbox.min.css') !!}">-->
@@ -43,7 +43,7 @@
 
     <!-- Fallback for browsers that don't support preload -->
     <noscript>
-        <link rel="stylesheet" href="{!! frontAssets('css/tiny-slider.min.css') !!}">
+        <!--<link rel="stylesheet" href="{!! frontAssets('css/tiny-slider.min.css') !!}">-->
         <link rel="stylesheet" href="{!! frontAssets('css/aos.min.css') !!}">
         <link rel="stylesheet" href="{!! frontAssets('css/flatpickr.min.css') !!}">
         <link rel="stylesheet" href="{!! frontAssets('css/glightbox.min.css') !!}">
@@ -52,7 +52,7 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     </noscript>
-
+    
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -78,22 +78,23 @@
         })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
     </script>
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>-->
-    <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
+    <script src="{!! frontAssets('js/jquery.min-3.6.js') !!}"></script>
     <script>
-        // Global AJAX error handler
-        $(document).ajaxError(function(event, jqXHR, settings, error) {
-            if (jqXHR.status === 419 || jqXHR.status === 401) {
-                // Session expired or CSRF token mismatch
-                window.location.href = "{{ route('front.index') }}";
-            }
-        });
-
-        // Add CSRF token to all AJAX requests
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
+        document.addEventListener('DOMContentLoaded', function() {
+            // Global AJAX error handler
+            $(document).ajaxError(function(event, jqXHR, settings, error) {
+                if (jqXHR.status === 419 || jqXHR.status === 401) {
+                    // Session expired or CSRF token mismatch
+                    window.location.href = "https://performancehealthsupport.com";
+                }
+            });
+    
+            // Add CSRF token to all AJAX requests
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
         });
     </script>
 	@stack('styles')
