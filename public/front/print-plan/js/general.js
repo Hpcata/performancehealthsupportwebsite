17d72b852204ed1609b70e2756f859fa4ee86157
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-02 17:29:03 +0530

    plan preview file design related changes

---
+++ public/front/print-plan/js/general.js
@@ -0,0 +1,47 @@
+/* Custom General jQuery
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+;(function($, window, document, undefined) {
+	//Genaral Global variables
+	"use strict";
+	var $win = $(window);
+	var $doc = $(document);
+	var $winW = function(){ return $(window).width(); };
+	var $winH = function(){ return $(window).height(); };
+	var $screensize = function(element){  
+			$(element).width($winW()).height($winH());
+		};
+		
+		var screencheck = function(mediasize){
+			if (typeof window.matchMedia !== "undefined"){
+				var screensize = window.matchMedia("(max-width:"+ mediasize+"px)");
+				if( screensize.matches ) {
+					return true;
+				}else {
+					return false;
+				}
+			} else { // for IE9 and lower browser
+				if( $winW() <=  mediasize ) {
+					return true;
+				}else {
+					return false;
+				}
+			}
+		};
+
+	$doc.ready(function() {
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+		// Remove No-js Class
+		$("html").removeClass('no-js').addClass('js');
+		
+		
+
+/*--------------------------------------------------------------------------------------------------------------------------------------*/		
+	});	
+
+/*All function nned to define here for use strict mode
+----------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+	
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+})(jQuery, window, document);
