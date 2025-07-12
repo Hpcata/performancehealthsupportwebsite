b4118cfac49e162baad5d9e925830cc08adb0248
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-12 12:23:08 +0530

    console log print remove and namesapce used

--- resources/views/backend/pages/plan/purchase-plan-create.blade.php
+++ resources/views/backend/pages/plan/purchase-plan-create.blade.php
@@ -1377,11 +1377,6 @@
             $('#allProteinTotal').text(`${Math.round(grandTotalProtein)}g`);
             $('#allFatTotal').text(`${Math.round(grandTotalFat)}g`);
             $('#allEnergyTotal').text(`${Math.round(grandTotalEnergy)}kJ`);
-
-            // console.log(`Total Nutrition Values:
-            // - Total Carb: ${grandTotalCarbs.toFixed(2)}g
-            // - Total Protein: ${grandTotalProtein.toFixed(2)}g
-            // - Total Fat: ${grandTotalFat.toFixed(2)}g`);
         }
 
         let userId = $('#user_id').val();
@@ -1401,7 +1396,6 @@
 
             const newMeals = currentSelectedMeals.filter(mealId => !oldMeals.includes(mealId));
             const unselectedMeals = oldMeals.filter(mealId => !currentSelectedMeals.includes(mealId));
-            // console.log(newMeals);
             previouslySelectedMeals[`${planId}_${mealTimeId}`] = currentSelectedMeals;
 
             // Remove unselected meals
@@ -1732,7 +1726,7 @@
                     });
                     // Store newOrder as needed (e.g., in a hidden input or JS variable)
                     // Example: window.currentItemOrder = newOrder;
-                    console.log('New order:', newOrder);
+                  
                 }
             });
 
@@ -2609,7 +2603,6 @@
 
         function parseFraction(value) {
             if (!value) return NaN;
-            console.log('val : ', value);
             value = String(value).trim(); // ✅ Convert to string safely
             if (value.includes('/')) {
                 const parts = value.split(' ');
@@ -2883,7 +2876,6 @@
                 // }
 
                 $('#editItemModal #ratio').val(ratio);
-                console.log(`Ratio compared to original row: ${ratio}`);
                 // Optionally: update nutrition or UI here
             });
 
@@ -3044,8 +3036,7 @@
                         const $updatedRow = $(`#itemRow_${planId}_${mealTimeId}_${mealId}_${itemId}`);
                         const $swapListItems = $updatedRow.find('td').eq(2).find('li[data-swap-item-id]');
                         // const ratio = parseFloat($('#editItemModal #ratio').val());
-                        // console.log($('#editItemModal #ratio').val());
-                        console.log('ration', ratio);
+                      
                         if(ratio != 0 && !isNaN(ratio)) {
                             $swapListItems.each(function () {
                                 const $swapLi = $(this);
@@ -3398,7 +3389,6 @@
                     }
                 }
             });
-            // console.log(qty);
             const carbs = parseFloat($('#editSwapItemModal #modalCarbs').text()) || 0;
             const protein = parseFloat($('#editSwapItemModal #modalProtein').text()) || 0;
             const fat = parseFloat($('#editSwapItemModal #modalFat').text()) || 0;
@@ -4334,7 +4324,6 @@
         });
 
         $('#addMoreSwapItemModal').on('hidden.bs.modal', function () {
-            console.log('Add More Swap Item Modal hidden');
 
             // Reset all inputs inside the modal
             $(this).find('input').val('');
