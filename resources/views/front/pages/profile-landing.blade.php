7f3e391110afd8c11c7fe28e66a5215f5647d397
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-15 14:31:11 +0530

    profile-landing page issue fixed

--- resources/views/front/pages/profile-landing.blade.php
+++ resources/views/front/pages/profile-landing.blade.php
@@ -25,14 +25,20 @@
             </div>
             <div class="welcome-arrow"></div>
         </section>
-
+        @if(isset($userPlan->plan))
         <!-- Sports Training Plan -->
         <section class="training-plan">
+            @if(isset($userPlan->plan))
             <div class="section-header">
-                <h2>{{ $userPlan->plan->name }}</h2>
-                <a href="/training-plan" class="see-all">See all</a>
+                <h2>{{ isset($userPlan->plan) ? $userPlan->plan->name : '' }}</h2>
+                <a href="{{ route('front.plans.details', ['id' => $userPlan->plan->id, 'user_id' => $userPlan->user->id]) }}" class="see-all">See Plan</a>
             </div>
-
+            @else
+            <div class="section-header">
+                <h2></h2>
+                <a href="#" class="see-all">See Plan</a>
+            </div>
+            @endif
             {{-- Tabs --}}
             <div class="tabs">
                 @php $firstTab = true; @endphp
@@ -59,8 +65,8 @@
                 @endforeach
             </div>
 
-            <div class="tab-content">
-                <div class="meal-cards" id="meal-cards-wrapper">
+            <div class="tab-content challenges">
+                <div class="challenge-cards" id="meal-cards-wrapper">
                     <p>Loading meals...</p>
                 </div>
             </div>
@@ -87,6 +93,7 @@
             </div> -->
         </section>
 
+        @endif
         <!-- Challenges -->
         <section class="challenges">
             <div class="section-header">
@@ -242,29 +249,33 @@
                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
                             class="consult-avatar"
                             alt="Kerry O'Bryan, expert coach avatar" />
-                        <span>Kerry O'Bryan • 60 min</span>
+                        <span style="padding-left:0">Kerry O'Bryan • 60 min</span>
                     </div>
                     <button class="btn-consult">Book consult</button>
                 </div>
             </div>
             <div class="consults-plans-grid">
                 <div class="plan-card-custom plan-competition">
-                    <div class="plan-title">Competition Plan</div>
-                    <div class="plan-desc">
-                        Unlock your best performance with a fully customised 24-hour competition day meal plan—designed to fuel you from the night before through recovery, tailored to your sport, your preferences, and your game-day goals.
+                    <div class="">
+                        <div class="plan-title">Competition Plan</div>
+                        <div class="plan-desc">
+                            Unlock your best performance with a fully customised 24-hour competition day meal plan—designed to fuel you from the night before through recovery, tailored to your sport, your preferences, and your game-day goals.
+                        </div>
                     </div>
-                    <div class="consult-user-row">
-                        <img
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
-                            class="consult-avatar"
-                            alt="Kerry O'Bryan, expert coach avatar" />
-                        <span>21 meals • 18 Nutrition tips</span>
+                    <div class="">
+                        <div class="consult-user-row">
+                            <img
+                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
+                                class="consult-avatar"
+                                alt="Kerry O'Bryan, expert coach avatar" />
+                                <img
+                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
+                                class="consult-avatar overlap1"
+                                alt="Kerry O'Bryan, expert coach avatar" />
+                            <span>21 meals • 18 Nutrition tips</span>
+                        </div>
+                        <button class="btn-consult">Learn more</button>
                     </div>
-                    <!-- <div class="plan-meta">
-                <i class="fa-solid fa-utensils"></i> 21 meals • 18 Nutrition
-                tips
-              </div> -->
-                    <button class="btn-consult">Learn more</button>
                 </div>
                 <div class="plan-card-custom plan-injury">
                     <div class="plan-title">Injury</div>
@@ -275,6 +286,10 @@
                         <img
                             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
                             class="consult-avatar"
+                            alt="Kerry O'Bryan, expert coach avatar" />
+                              <img
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=40&h=40&fit=crop&crop=face"
+                            class="consult-avatar overlap1"
                             alt="Kerry O'Bryan, expert coach avatar" />
                         <span>21 meals • 18 Nutrition tips</span>
                     </div>
@@ -315,7 +330,7 @@
                                 class="channel-avatar" />
                             <div class="channel-name">
                                 <div class="channel-name-main">
-                                    <label>surfboard_co</label>
+                                     <label class="insta-handle-name">surfboard_co</label>
                                     <img src="{{ frontAssets('images/verified.webp') }}" alt="Verified badge" width="16" height="16" />
                                 </div>
                                 <label>Turnstile . LIGHT DESIGN</label>
@@ -326,8 +341,10 @@
                                 Learn about the best surfboard techniques for beginners and
                                 pros alike.
                             </p>
-                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="32" height="32" />
-                            <span class="likes">345 likes</span>
+                          <div class="insta-like-wrapper">
+                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="20" height="18" style="width:20px;"/>
+                            <span class="likes">892 likes</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -359,7 +376,7 @@
                                 class="channel-avatar" />
                             <div class="channel-name">
                                 <div class="channel-name-main">
-                                    <label>surfboard_co</label>
+                                    <label class="insta-handle-name">surfboard_co</label>
                                     <img src="{{ frontAssets('images/verified.webp') }}" alt="Verified badge" width="16" height="16" />
                                 </div>
                                 <label>Turnstile . LIGHT DESIGN</label>
@@ -370,8 +387,10 @@
                                 Learn about the best surfboard techniques for beginners and
                                 pros alike.
                             </p>
-                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="32" height="32" />
-                            <span class="likes">567 likes</span>
+                           <div class="insta-like-wrapper">
+                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="20" height="18" style="width:20px;"/>
+                            <span class="likes">892 likes</span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -403,7 +422,7 @@
                                 class="channel-avatar" />
                             <div class="channel-name">
                                 <div class="channel-name-main">
-                                    <label>surfboard_co</label>
+                                    <label class="insta-handle-name">surfboard_co</label>
                                     <img src="{{ frontAssets('images/verified.webp') }}" alt="Verified badge" width="16" height="16" />
                                 </div>
                                 <label>Turnstile . LIGHT DESIGN</label>
@@ -414,8 +433,10 @@
                                 Learn about the best surfboard techniques for beginners and
                                 pros alike.
                             </p>
-                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="32" height="32" />
+                            <div class="insta-like-wrapper">
+                            <img src="{{ frontAssets('images/like.webp') }}" alt="Like icon" width="20" height="18" style="width:20px;"/>
                             <span class="likes">892 likes</span>
+                            </div>
                         </div>
                     </div>
                 </div>
