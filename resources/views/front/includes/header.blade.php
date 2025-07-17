0888826bf9a15941f3a3e1b0c76d12b925db52d9
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-17 14:35:05 +0530

    index page login modal popup not open issue fixed

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -119,9 +119,43 @@ $auth = auth()->guard('web')->check();
         </nav>
     </div>
 </header>
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="loginModalLabel">Sign In</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            
+            <div class="modal-body">
+                <div id="login-error" class="text-danger"></div> <!-- This will display the error message -->
+                <!-- Sign In Form -->
+                <form id="login-form">
+                    <div class="mb-3">
+                        <label for="login-email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="login-email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="login-password" class="form-label">Password</label>
+                        <input type="password" name="password" class="form-control" id="login-password" required>
+                    </div>
 
+                    <!-- Sign In Button -->
+                    <button type="submit" id="login-submit" class="btn btn-primary w-100 mt-3">
+                        Sign In
+                    </button>
+                </form>
+
+                <!-- Sign Up Link -->
+                <div class="mt-3 text-center">
+                    <small>Don't have an account? <a href="#" id="show-signup-modal">Sign Up</a></small>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 @endif
-
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     function handleLogout(event) {
         event.preventDefault();
