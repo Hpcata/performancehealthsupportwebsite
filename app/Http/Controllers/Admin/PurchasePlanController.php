f6d090d04af2d1b3a9b276517594f94de4de8370
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-18 09:16:36 +0530

    qty update issue fixed

--- app/Http/Controllers/Admin/PurchasePlanController.php
+++ app/Http/Controllers/Admin/PurchasePlanController.php
@@ -3460,7 +3460,7 @@ class PurchasePlanController extends Controller
                 // dd($selectedQty);
                 foreach ($selectedQty as $unitData) {
                     $originalQty = floatval($unitData['qty']);
-                    $adjustedQty = $originalQty / floatval($ratio);
+                    $adjustedQty = $originalQty * floatval($ratio);
 
                     // Optionally round or format:
                     $adjustedQty = round($adjustedQty, 2); // keep 2 decimal places
@@ -3489,7 +3489,7 @@ class PurchasePlanController extends Controller
                 // dd($selectedQty);
                 foreach ($selectedQty as $unitData) {
                     $originalQty = floatval($unitData['qty']);
-                    $adjustedQty = $originalQty / floatval($ratio);
+                    $adjustedQty = $originalQty * floatval($ratio);
     
                     // Optionally round or format:
                     $adjustedQty = round($adjustedQty, 2); // keep 2 decimal places
