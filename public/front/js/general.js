b9c7d2a5a0f7f1bdef73629a7d69d3d378b4eb1b
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-18 09:12:38 +0530

    =generel.js file booking image url set

--- public/front/js/general.js
+++ public/front/js/general.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
                                     <a href="https://booking.biohealthpassport.com.au/kerry-obryan">
                                         `;
                                         html += `<div class="booking-type-box">
-                                        <img src="${item.thumbnail_image}" alt="${item.name}" class="img-fluid">`;
+                                        <img src="https://booking.biohealthpassport.com.au/${item.thumbnail_image}" alt="${item.name}" class="img-fluid">`;
                                         if(item.description != null){
                                         html += `<div class="booking-desc-box">
                                                 ${item.description}
