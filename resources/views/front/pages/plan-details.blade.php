6bf607d9021137d648f7afdb14614cf89f01d714
kartikvadhaiya <kartik@catalysta.com.au>
2025-07-18 10:42:17 +0530

    plan detail page modal popup issue fixed

--- resources/views/front/pages/plan-details.blade.php
+++ resources/views/front/pages/plan-details.blade.php
@@ -316,6 +316,47 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="ShoppingModal" tabindex="-1" aria-labelledby="ShippingModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ShippingModalLabel">Shopping List</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="ingredient-list">
+                        
+                    </div>
+                </div>
+                <div class="modal-footer p-0">
+                    <a href="javascript:void(0);" class="btn btn-primary m-0 w-100 text-center rounded-0" data-bs-target="#ShippingPrintModal" data-bs-toggle="modal">Print Shopping List Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Shoping print Modal -->
+    <div class="modal fade" id="ShippingPrintModal" tabindex="-1" aria-labelledby="ShippingPrintModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ShippingPrintModalLabel">Shopping List</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="print-list">
+                        <ul>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer p-0">
+                    <button type="button" class="btn btn-primary m-0 w-100 text-center rounded-0" data-bs-dismiss="modal">Print</button>
+                </div>
+            </div>
+        </div>
+    </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
